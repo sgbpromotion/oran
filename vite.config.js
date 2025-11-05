ee0cf0a588c48fb964a829ef10a05fc445c34a17
sgbpromotion <contact@sgbpromotion.com>
2025-11-05 02:08:02 +0100

    fix: adjust vite base path for production

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ✅ ajoute cette ligne ici
+  base: '', // important : vide, pas "./"
+  build: {
+    outDir: 'dist',
+  },
 })
